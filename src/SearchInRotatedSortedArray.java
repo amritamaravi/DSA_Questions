78657f4800d0935337b311a893908844bf1dbfff
Amrita Maravi <amritamaravi09@gmail.com>
2025-08-31 00:00:20 +0530

    find  mini in rotated sorted array

--- src/SearchInRotatedSortedArray.java
+++ src/SearchInRotatedSortedArray.java
@@ -1,6 +1,6 @@
 public class SearchInRotatedSortedArray {
     public static void main(String[] args) {
-int[] nums = {4,5,6,7,0,1,2};
+        int[] nums = {4,5,6,7,0,1,2};
         System.out.println(search(nums, 0));
     }
     static int search(int[] nums, int target) {
